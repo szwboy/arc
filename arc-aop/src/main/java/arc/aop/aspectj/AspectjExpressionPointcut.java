93698e406728d79b73f9bd4b7387831c9720d43b
szwboy <szwboy@hotmail.com>
2015-10-20 23:42:07 +0800

    aop aspectj

--- arc-aop/src/main/java/arc/aop/aspectj/AspectjExpressionPointcut.java
+++ arc-aop/src/main/java/arc/aop/aspectj/AspectjExpressionPointcut.java
@@ -4,6 +4,11 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.aspectj.weaver.patterns.NamePattern;
+import org.aspectj.weaver.tools.ContextBasedMatcher;
+import org.aspectj.weaver.tools.FuzzyBoolean;
+import org.aspectj.weaver.tools.MatchingContext;
+import org.aspectj.weaver.tools.PointcutDesignatorHandler;
 import org.aspectj.weaver.tools.PointcutExpression;
 import org.aspectj.weaver.tools.PointcutParameter;
 import org.aspectj.weaver.tools.PointcutParser;
@@ -28,6 +33,10 @@ public class AspectjExpressionPointcut implements ExpressionPointcut, ClassFilte
 		supportedPointcutKinds.add(PointcutPrimitive.THIS);
 		supportedPointcutKinds.add(PointcutPrimitive.WITHIN);
 		supportedPointcutKinds.add(PointcutPrimitive.TARGET);
+		supportedPointcutKinds.add(PointcutPrimitive.AT_ANNOTATION);
+		supportedPointcutKinds.add(PointcutPrimitive.AT_WITHIN);
+		supportedPointcutKinds.add(PointcutPrimitive.AT_ARGS);
+		supportedPointcutKinds.add(PointcutPrimitive.AT_TARGET);
 	}
 	
 	public AspectjExpressionPointcut(Class<?> pointcutDeclarationScope, String[] paramNames, Class<?>[] paramTypes){
@@ -45,11 +54,15 @@ public class AspectjExpressionPointcut implements ExpressionPointcut, ClassFilte
 		return parser.parsePointcutExpression(getExpression(), pointcutDeclarationScope, pointcutParams);
 	}
 	
+	/**
+	 * initialize a pointcut parser and register the new added primitive type component
+	 * @return
+	 */
 	private PointcutParser initializePointcutParser(){
 		PointcutParser parser= PointcutParser.
 				getPointcutParserSupportingSpecifiedPrimitivesAndUsingSpecifiedClassLoaderForResolution(
 						supportedPointcutKinds, componentClassLoader);
-		
+		parser.registerPointcutDesignatorHandler(new ComponentNamePointcutDesignatorHandler());
 		return parser;
 	}
 	
@@ -86,6 +99,63 @@ public class AspectjExpressionPointcut implements ExpressionPointcut, ClassFilte
 	public boolean matches(Class<?> clz) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	/**
+	 * to handle a new add aspectj primitive type component
+	 * @author sunzhongwei
+	 *
+	 */
+	private class ComponentNamePointcutDesignatorHandler implements PointcutDesignatorHandler{
+
+		private final static String COMPONENT_DESIGNATOR_NAME= "component";
+		@Override
+		public String getDesignatorName() {
+			return COMPONENT_DESIGNATOR_NAME;
+		}
+
+		@Override
+		public ContextBasedMatcher parse(String arg0) {
+			return new ComponentNameContextBaseMatcher(arg0);
+		}
+		
+	}
+	
+	private class ComponentNameContextBaseMatcher implements ContextBasedMatcher{
+		private NamePattern expressionPattern;
+		
+		ComponentNameContextBaseMatcher(String expression){
+			this.expressionPattern= new NamePattern(expression);
+		}
+
+		@Override
+		public boolean couldMatchJoinPointsInType(Class arg0) {
+			return false;
+		}
+
+		@Override
+		public boolean couldMatchJoinPointsInType(Class arg0,
+				MatchingContext arg1) {
+			return false;
+		}
+
+		@Override
+		public boolean matchesDynamically(MatchingContext arg0) {
+			return true;
+		}
+
+		@Override
+		public FuzzyBoolean matchesStatically(MatchingContext arg0) {
+			String advisedComponentName= null;
+			
+			return FuzzyBoolean.fromBoolean(expressionPattern.matches(advisedComponentName));
+		}
+
+		@Override
+		public boolean mayNeedDynamicTest() {
+			return false;
+		}
+		
 	}
 
 }
